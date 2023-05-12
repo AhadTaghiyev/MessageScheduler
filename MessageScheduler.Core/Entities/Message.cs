e5e3f4856200483ef50ceaad26c978416da75169
AhadTaghiyev <ahad.taghiyev@gmail.com>
2023-05-12 16:29:45 +0400

    added core libary

---
+++ MessageScheduler.Core/Entities/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageScheduler.Core.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string To { get; set; }
+        public string Content { get; set; }
+        public DateTime SendAt { get; set; }
+        public string Method { get; set; }
+    }
+}
