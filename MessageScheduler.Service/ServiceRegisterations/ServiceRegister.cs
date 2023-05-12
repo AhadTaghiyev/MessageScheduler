031c13bea7918c614924a0939c42bff29ef64125
AhadTaghiyev <ahad.taghiyev@gmail.com>
2023-05-12 16:55:57 +0400

    added ServiceRegisteration

---
+++ MessageScheduler.Service/ServiceRegisterations/ServiceRegister.cs
@@ -0,0 +1,19 @@
+﻿using MessageScheduler.Data.Contexs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+namespace MessageScheduler.Service.ServiceRegisterations
+{
+    public static class ServiceRegister
+    {
+        public static void AddServiceRegisteration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MessageSchedulerDbContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("Default"));
+            });
+        }
+    }
+}
