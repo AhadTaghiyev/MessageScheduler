6832268f4cdac8c7ed034ac7e1f2ac8d07360915
AhadTaghiyev <ahad.taghiyev@gmail.com>
2023-05-12 17:52:06 +0400

    configuration date time

--- MessageScheduler.Data/Contexts/MessageSchedulerDbContext.cs
+++ MessageScheduler.Data/Contexts/MessageSchedulerDbContext.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MessageScheduler.Core.Entities.BaseEntities;
+using Microsoft.EntityFrameworkCore;
 
 namespace MessageScheduler.Data.Contexs
 {
     public class MessageSchedulerDbContext : DbContext
     {
         public MessageSchedulerDbContext(DbContextOptions options):base(options) { }
-       
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker
+                 .Entries<BaseEntity>();
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow.AddHours(4),
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow.AddHours(4),
+                    _ => DateTime.UtcNow
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
     }
 }
